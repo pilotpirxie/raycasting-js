93d927f2b31f36c2be446a6a21810f3400bff52a
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-09-06 00:46:02 +0200

    Add animation
    Draw line to mouse position

--- game.js
+++ game.js
@@ -5,28 +5,54 @@ function clearScreen() {
 }
 
 function drawWalls() {
-    let i = 0;
     for (let y = 0; y < MAP_HEIGHT; y++) {
         for (let x = 0; x < MAP_WIDTH; x++) {
-            i++;
-            setTimeout(() => {
-                if (MAP[y][x] === 1) {
-                    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-                    ctx.strokeRect(WALL_SIZE * x, WALL_SIZE * y, WALL_SIZE, WALL_SIZE);
-                    console.log(x, y, WALL_SIZE * x, WALL_SIZE * y, WALL_SIZE * (x+1), WALL_SIZE * (y+1));
-                }
-            }, i * 5);
+            if (MAP[y][x] === 1) {
+                ctx.strokeRect(WALL_SIZE * x, WALL_SIZE * y, WALL_SIZE, WALL_SIZE);
+                // console.log(x, y, WALL_SIZE * x, WALL_SIZE * y, WALL_SIZE * (x+1), WALL_SIZE * (y+1));
+            }
         }
     }
+}
+
+function drawPlayer() {
+    ctx.beginPath();
+    ctx.arc(playerX * WALL_SIZE, playerY * WALL_SIZE, WALL_SIZE/2, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+function drawPlayerFov() {
+    ctx.beginPath();
+    ctx.moveTo(playerX * WALL_SIZE, playerY * WALL_SIZE);
+    ctx.lineTo(mouseX, mouseY);
+    ctx.stroke();
+}
+
+function showDebug() {
+    console.log(mouseX, mouseY, fps);
 }
 
 function draw() {
     clearScreen();
     drawWalls();
+    drawPlayer();
+    drawPlayerFov();
+    showDebug();
 }
 
-function animate() {
+function animate(timestamp) {
     draw();
+
+    let secondsPassed = (timestamp - previousFrameTime) / 1000;
+    fps = Math.round(1 / secondsPassed);
+    previousFrameTime = timestamp;
+
+    window.requestAnimationFrame(animate);
 }
 
 window.requestAnimationFrame(animate);
+
+canvas.addEventListener('mousemove', event => {
+    mouseX = event.offsetX;
+    mouseY = event.offsetY;
+});
--- settings.js
+++ settings.js
@@ -10,3 +10,12 @@ const WALL_SIZE = Math.min(HEIGHT/MAP_HEIGHT, WIDTH/MAP_WIDTH);
 
 let playerX = 10;
 let playerY = 10;
+
+let cameraX = 10;
+let cameraY = 50;
+
+let mouseX = 0;
+let mouseY = 0;
+
+let previousFrameTime = 0;
+let fps;
